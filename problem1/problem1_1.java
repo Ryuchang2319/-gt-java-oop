4b348f7063ed28282d7075077de9d03c47f92da4
Ryuchang2319 <xxioryuchang@gmail.com>
2026-09-12 21:02:49 +0000

    Save Java practice

---
+++ problem1/problem1_1.java
@@ -0,0 +1,9 @@
+package problem1;
+
+public class problem1_1{
+
+    public static void main(String[] args) {
+        System.out.println("山田太郎");
+
+    }
+}
---
+++ problem1/problem1_4.java
@@ -0,0 +1,9 @@
+package problem1;
+
+public class problem1_4 {
+
+    public static void main(String[] args) {
+        System.out.println(1 + " + " + 2 + " + " + 3 + " = " + (1+2+3));
+    }
+
+}
